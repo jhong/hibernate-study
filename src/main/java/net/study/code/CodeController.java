c47d88f7998cf0815ac5aff8794b64b773a87a95
Jihong Kim <jhkim@ecplaza.net>
2015-07-30 10:48:08 +0900

    - logger 적용 (slf4j, log4j) - 전체 코드 갯수 화면에 보이기 : Code Controller 에서 직접 ICodeDao 사용하도록 구현 및 테스트

--- src/main/java/net/study/code/CodeController.java
+++ src/main/java/net/study/code/CodeController.java
@@ -1,5 +1,9 @@
 package net.study.code;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CodeController {
 
+	@Autowired
+	private ICodeDao codeDao;
+	
     /**
 	 * <pre>
 	 * 목록 페이지 조회
@@ -18,9 +25,12 @@ public class CodeController {
      */
     @RequestMapping("/code")
 	public String viewList (ModelMap model) throws Exception {
-    	model.addAttribute("codeName", "abcde");
     	
-    	// /WEB-INF/jsp/code/code_list.jsp
+    	Map condition = new HashMap();
+    	int totalRow = codeDao.selectListCount(condition);
+
+    	model.addAttribute("codeName", "abcde");
+    	model.addAttribute("totalRow", totalRow);
 		return "code/code_list";
 	}
 
